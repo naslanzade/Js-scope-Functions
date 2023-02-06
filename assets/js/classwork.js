007f04d7ac2642dcff469672c07b5236cbef67bb
naslanzade <novrastafa@code.edu.az>
2023-02-06 23:15:30 +0400

    add folder with homework

---
+++ assets/js/classwork.js
@@ -0,0 +1,215 @@
+"use strict"
+
+//Classwork Practice
+
+
+// let numbers=[2,3,4,5,6,7,8,9,11];
+
+// for(let i=o; i<numbers.length;i++){
+//     if(numbers[I]==7){
+//         break;
+//     }else{
+//         console.log(numbers[i])
+//     }
+// }
+
+
+
+// let numbers=[6,7,8,9,,"Resul",11];
+
+// for(let i=o; i<numbers.length;i++){
+
+//     if(!isNan(numbers[i])){
+
+//         if(numbers[i]%2==0){
+//             console.log(numbers[i])
+//         }
+//     }
+// }
+
+
+
+// let numbers=[6,7,8,9,,"20",11];
+
+// for(let i=o; i<numbers.length;i++){
+
+//     if(!isNan(numbers[i]) && typeof(numbers[i])=="number"){
+
+//         if(numbers[i]%2==0){
+//             console.log(numbers[i])
+//         }
+//     }
+// }
+
+
+// let surname="test";
+// if(typeof surname=="string"){
+//     console.log("Yes string")
+// }else{
+//     console.log("No string")
+// }
+
+
+// try {
+
+
+//     let isMarried=true;
+//     if(isMarried){
+//         let result="Married";
+
+//     }
+
+//     console.log(result)
+    
+//   } catch (error) {
+
+//     console.log(error.message)   
+//   } 
+//   finally{
+//     console.log("Welcome")
+//   }
+
+
+// let isMarried=false;
+
+// if(!isMarried){
+//     let text="Not married";
+// }
+
+
+
+// let name ="Text"
+// function showText(name){
+
+//     let surname="Text1"
+//     console.log(surname + " " + name)
+
+// }
+
+// showText(name);
+
+
+// let name ="Text"
+// function showText(name){
+
+   
+//     return surname + " " + name
+
+// }
+
+// console.log(showText(name,surname));
+
+
+
+
+// let name ="Text"
+// let surname="Text1"
+// function showText(name){
+
+//    let isMarried=false;
+//    if(isMarried){
+//     return surname + " " + name + " " + isMarried + " Siz sayta qeydiyyat ede bilersiniz";
+//    }else{
+//     return surname + " " + name + " " + isMarried + " Siz sayta qeydiyyat ede bilmersiniz";
+//    }
+    
+// }
+
+// let result=showText(name,surname)
+
+// console.log(result);
+
+
+
+// function sumOfNumbers(a,b,c){
+//     return a+b+c;
+// }
+
+// console.log(sumOfNumbers(1,4,6))
+
+
+// function sumOfNumbers(a,b,c){
+//    console.log(arguments)
+// }
+
+// sumOfNumbers(1,2,3,4,5,6)
+
+
+
+// function showFullData(){
+//     console.log(this);
+// }
+
+// showFullData();
+
+// let obj={
+//     name:"XXX",
+//     surname:"YYY",
+//     getData:function(){
+//         console.log(this)
+//     }
+// }
+
+// obj.getData();
+
+
+
+// let nums=[1,2,3,4,5,6,8,9];
+
+// function showNumbers(arr){
+
+//     for(let i=0;i<arr.length;i++){
+//         console.log(arr[i])
+//     }
+
+// }
+
+// showNumbers(nums);
+
+
+
+// let nums=[1,2,3,4,5,6,8,9];
+
+// function showNumbers(arr){
+
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]==8){
+//             console.log(arr[i]);
+//             return;
+//         }
+//     }
+
+//     console.log("Welcome")
+
+// }
+
+// showNumbers(nums);
+
+
+
+
+// let nums=[1,2,3,4,5,6,8,9];
+
+// function showNumbers(arr){
+
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]==8){
+//             console.log(arr[i]);
+//             break;
+//         }else{
+//             console.log(arr[i]);
+//         }
+//     }
+
+//     console.log("Welcome")
+
+// }
+
+// showNumbers(nums);
+
+
+// let showEmail=()=>{
+//     console.log("code@gmail.com")
+// }
+
+// showEmail();
---
+++ assets/js/homework.js
@@ -0,0 +1,118 @@
+"use strict"
+
+
+//Task-1
+// let num;
+// function divisionOfNumber(num){
+//     if(num%3==0 & num%7==0){
+//         console.log(num);
+//     }else{
+//         console.log("Please enter correct number");
+//     }
+// }
+
+// divisionOfNumber(49);
+
+
+
+//Task-2
+// let factorial=1;
+// function calculationOfFactorial(num){
+//     if(num<0){
+//         return -1;
+//     }
+//     else{
+//         for(let i=num;i>=2;i--){ 
+//            factorial=factorial*i;
+
+//         }
+//         return factorial;
+//     }
+// }
+
+// let num=4;
+// factorial=calculationOfFactorial(num);
+// console.log("Factorial of " + num + " : " + factorial);
+
+
+
+
+//Task-3
+// let arr = [1, 2, 3, 4, 5,6]
+// let sum = 0;
+
+// function squareEvenNumbers(arr) {
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (arr[i] % 2 == 0) {
+//             sum +=Math.pow(arr[i], 2)
+//         }
+//     }
+
+//     return sum;
+// }
+
+// console.log("Sum of square of even numbers is:" + " " + squareEvenNumbers(arr));
+
+
+
+
+//Task-4
+// let email;
+// let password;
+// function checkUser(email,password){
+//     if(email=="cavid@code.edu.az" && password=="12345"){
+//         console.log("Success")
+//     }else{
+//         console.log("Mail or password is wrong")
+//     }
+// }
+
+// checkUser("cavid@code.edu.az","12345");
+
+
+//Task-5
+// let arr = [1,2,3,5,6,7,8]
+// let sum=0;
+
+// function sumOddNumbers(arr) {
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (arr[i] % 2!=0) {
+//             sum +=arr[i]
+//         }
+//     }
+//     return sum;
+// }
+
+// console.log("Sum of odd numbers is:" + " " + sumOddNumbers(arr));
+
+
+//Task-6
+// let arr = [1,2,4,3,5,6,7,8]
+// let count=0;
+
+// function countEvenNumbers(arr) {
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (arr[i] % 2==0) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log("Count of even numbers is:" + " " + countEvenNumbers(arr));
+
+
+
+
+
+
+
+
+
+
